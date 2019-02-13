59337e099c8b8cd391072823de3a61cbc7b6e266
风想飘 <fengxiangpiao@hotmail.com>
2019-02-13 13:01:10 +0800

    修改machine

--- automachine.py
+++ automachine.py
@@ -1,28 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import wx
-import mainwin
+import sys
 import smbus2
 import requests
 import Adafruit_CharLCD
 import socket
 import time
-import numpy
-# 首先，咱们从刚刚源文件中将主窗体继承下来.就是修改过name属性的主窗体咯。
-class MainWindow(mainwin.MainWin):
-    # 咱们给个初始化函数，将文本框初始填有‘主窗口测试’几个字
-    # 不能直接覆盖原有__ini__方法，这样会导致窗体启动失败。咱们新建一个，然后再调用
-    def init_main_window(self):
-        self.text_main.SetValue('主窗口测试')
-    # 将点击按钮清空文本框的,功能写成函数
-    def main_button_click(self, event):
-        self.text_main.Clear()
+from PyQt5.QtWidgets import QApplication, QWidget
+
 if __name__ == '__main__':
-    app = wx.App()
-    # None表示的是此窗口没有上级父窗体。如果有，就直接在父窗体代码调用的时候填入‘self’就好了。
-    main_win = mainwin.MainWin(None)
-    main_win.Show()
-    app.MainLoop()
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    sys.exit(app.exec_())
 
     bus = smbus2.SMBus(1)
     address = 0x04
