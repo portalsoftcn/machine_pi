3964019fe7727d71d4264d07ac46c2e7eea477c9
风想飘 <fengxiangpiao@hotmail.com>
2019-02-13 16:21:16 +0800

    修改窗口尺寸

--- automachine.py
+++ automachine.py
@@ -8,9 +8,13 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    desktop = QApplication.desktop();
+    screenRect = desktop.screenGeometry()
+    screenW = screenRect.width()
+    screenH = screenRect.height()
     w = QWidget()
-    w.resize(800, 600)
-    w.move(300, 300)
+    w.resize(screenW, screenH)
+    #w.move(screenH/2, 300)
     w.setWindowTitle('磨机辅助控制系统')
     w.show()
     sys.exit(app.exec_())
