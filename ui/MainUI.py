a0530afe2961d9d4fadc85fab8be90d99a9bc41a
lvchenghui <fengxiangpiao@hotmail.com>
2019-02-14 14:39:08 +0800

    modify distance

--- ui/MainUI.py
+++ ui/MainUI.py
@@ -25,18 +25,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.address = 0x04
         self.bus = smbus2.SMBus(1)
+
+    def getDistance(self):
+        byteList = self.bus.read_i2c_block_data(self.address,0,6)
+        distanceStr = ""
+        for a in range(2):
+            id = byteList[a * 3]
+            distance = byteList[a * 3 + 1] * 256 + byteList[a * 3 + 2]
+            distanceStr += str(id) + ":" + str(distance) + " "
+        print (distanceStr)
+    
     @pyqtSlot()
     def on_btnOilStop_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        print(" stop ")
         self.bus.write_byte(self.address,22)
     @pyqtSlot()
     def on_btnOilStart_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
         self.bus.write_byte(self.address,21)
+        self.getDistance()
